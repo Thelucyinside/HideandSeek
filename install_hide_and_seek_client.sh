c134d516861b9988c297c02937f280f1dec887ca
Thelucyinside <107397322+Thelucyinside@users.noreply.github.com>
2025-05-31 10:30:44 +0200

    Update install_hide_and_seek_client.sh

--- install_hide_and_seek_client.sh
+++ install_hide_and_seek_client.sh
@@ -26,10 +26,8 @@ read -r
 # --- Wichtige Einstellungen (bitte vorab anpassen!) ---
 # !!! ÄNDERE DIESE ZEILE ZU DEINEM AKTUELLEN GITHUB REPOSITORY !!!
 # Beispiel: GITHUB_REPO_URL="https://github.com/DeinUsername/DeinCoolesSpiel.git"
-GITHUB_REPO_URL="https://github.com/DEIN_BENUTZERNAME/DEIN_REPOSITORYNAME.git" # <--- HIER DEINE URL EINTRAGEN!
-
-GAME_INSTALL_DIR="$HOME/hide-and-seek-client" # Wo das Spiel gespeichert wird
-START_COMMAND_NAME="hide-and-seek"            # So startest du das Spiel später
+GITHUB_REPO_URL="https://github.com/Thelucyinside/HideandSeek.git" # <--- HIER DEINE URL EINTRAGEN!L_DIR="$HOME/hide-and-seek-client" # Wo das Spiel gespeichert wird
+START_COMMAND_NAME="start"            # So startest du das Spiel später
 LOCAL_VERSION_FILE_NAME=".version_hs"         # Kleine Datei für die Versionsnummer
 REMOTE_VERSION_FILE_NAME="VERSION.txt"        # Diese Datei muss in deinem GitHub-Repo sein
 
